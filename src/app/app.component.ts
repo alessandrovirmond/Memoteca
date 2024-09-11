fd535a44e63e25ce83921255fe3b3f10adb964bc
Alessandro Virmond <dev6@rfidbrasil.com>
2024-09-11 17:59:08 -0300

    footer component criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrl: './criar-pensamentos.component.css'
+})
+export class CriarPensamentosComponent {
+
+}
